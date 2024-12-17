d4bcb73ec77ea2707554a1939e1e45456381e015
Shun Fujimura <j3361ohr@sit.ac.jp>
2024-12-17 16:24:51 +0900

    GPU で実行する

--- 後期プログラミング演習/models.py
+++ 後期プログラミング演習/models.py
@@ -20,12 +20,18 @@ class MyModel(nn.Module):
         logits = self.network(x)
         return logits
 
-def test_accuracy(model,dataloader):
+def test_accuracy(model,dataloader,device='cpu'):
     # 全てのミニバッチに対して推論をして、正解率を計算する
     n_corrects = 0 #正解した個数
 
+    model.to(device)
+
     model.eval()
     for image_batch,label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+
+
         # モデルに入れて結果 (logits) を出す
         with torch.no_grad():
             logits_batch = model(image_batch)
@@ -39,10 +45,16 @@ def test_accuracy(model,dataloader):
     return accuracy
 
 
-def train(model,dataloader,loss_fn,optimizer):
+def train(model,dataloader,loss_fn,optimizer,device='cpu'):
     """l epoch の学習を行う"""
+
+    model.to(device)
+
     model.train()
     for image_batch,label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+
         # モデルにバッチを入れて計算
         logits_batch = model(image_batch)
 
@@ -60,11 +72,17 @@ def train(model,dataloader,loss_fn,optimizer):
 
 
 
-def test(model,dataloader,loss_fn):
+def test(model,dataloader,loss_fn,device='cpu'):
     loss_total = 0.0
+
+    model.to(device)
 
     model.eval()
     for image_batch,label_batch in dataloader:
+        image_batch.to(device)
+        label_batch.to(device)
+
+
         with torch.no_grad():
             logits_batch = model(image_batch)
         
